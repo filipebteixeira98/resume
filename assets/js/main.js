21f37144db6a607f09f5037b7ba3cc8c15da99bb
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-01-28 23:15:49 -0300

    chore: created gsap animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,45 @@ const showMenu = (toggleId, navId) => {
 };
 
 showMenu('nav-toggle', 'nav-menu');
+
+gsap.to('.first', 1.5, { delay: 0.5, top: '-100%', ease: Expo.easeInOut });
+gsap.to('.second', 1.5, { delay: 0.7, top: '-100%', ease: Expo.easeInOut });
+gsap.to('.third', 1.5, { delay: 0.9, top: '-100%', ease: Expo.easeInOut });
+gsap.from('.home__img', { opacity: 0, duration: 2, delay: 2, x: 60 });
+gsap.from('.home__information', {
+  opacity: 0,
+  duration: 3,
+  delay: 2.3,
+  y: 25,
+});
+gsap.from('.anime-text', {
+  opacity: 0,
+  duration: 3,
+  delay: 2.3,
+  y: 25,
+  ease: 'expo.out',
+  stagger: 0.3,
+});
+gsap.from('.nav__logo', {
+  opacity: 0,
+  duration: 3,
+  delay: 3.2,
+  y: 25,
+  ease: 'expo.out',
+});
+gsap.from('.nav__item', {
+  opacity: 0,
+  duration: 3,
+  delay: 2.3,
+  y: 25,
+  ease: 'expo.out',
+  stagger: 0.2,
+});
+gsap.from('.home__social-icon', {
+  opacity: 0,
+  duration: 3,
+  delay: 4,
+  y: 25,
+  ease: 'expo.out',
+  stagger: 0.2,
+});
